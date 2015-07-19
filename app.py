e04de9d867b5e81d91b4029ffbdc583bac55a2d9
Francisco Souza <f@souza.cc>
2015-07-19 12:38:43 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html", **os.environ)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "5000"))
+    debug = os.environ.get("DEBUG") in ("True", "true", "t", "1")
+    app.run(host="0.0.0.0", port=port, debug=debug)
